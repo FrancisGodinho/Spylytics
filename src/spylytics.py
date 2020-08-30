fc6f98e7ca6feab390e39ba87a43b538dc7fce01
francisgodinho <francisgodinho2010@hotmail.com>
2020-08-30 14:40:13 -0700

    seperated the model and prediction

--- src/spylytics.py
+++ src/spylytics.py
@@ -6,86 +6,93 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import MinMaxScaler
 import math
 
+"""
+S&P500: beta = 1
 
-def create_datasets(close_prices, price_history_len):
+BETA < -1:
+    AHPI: beta = -5.25
+    ELTK: beta = -2.76
 
-    train_data_len = math.ceil(len(close_prices) * 0.8)
+BETA ~ -1:
+    RKDA: beta = -0.82
+    DXR: beta = -1.02
+    ZM: beta = -1.51
+
+BETA ~ 0:
+    AEY: beta = 0.04
+    ADSW: beta = 0.27
+
+BETA ~ 1:
+    AAPL: beta = 1.23
+    ACN: beta = 1.02
+    NFLX: beta = 0.97
+    ENPH: beta = 1.01
+
+BETA > 1:
+    RCL: beta = 2.57
+    URI: beta = 2.40
+    DXC: beta = 2.41
+"""
+
+SCALER = MinMaxScaler(feature_range=(0, 1)) 
+
+def main():
+    tickers = ["S&P500", "AHPI", "ELTK", "RKDA", "DXR", "ZM", "AEY", "ADSW", "AAPL", "ACN", "NFLX", "ENPH", "RCL", "URI", "DXC"]
+    losses = []
+    
+    for ticker in tickers:
+        losses.append(predict_stock(ticker))
+    
+    for ticker, loss in zip(tickers, losses):
+        print(f'{ticker}, Loss: {"{:.2e}".format(loss)}')
+
+def predict_stock(ticker):
+    model = keras.models.load_model("./models/spylytics.h5")
+    
+    df, x, y = load_data(file=f"../data/{ticker}.csv", num_days=1095)
+
+    x = np.reshape(x, (x.shape[0], x.shape[1], 1))
+    loss = model.evaluate(x, y)
+
+    predictions = model.predict(x)
+    predictions = SCALER.inverse_transform(predictions)
+    
+    #plot_results(df[60:], predictions)
+    return loss
+
+def load_data(file, num_days):
+
+    df = pd.read_csv(file)
+    df.drop(['Open', 'High', 'Low', 'Adj Close', 'Volume'], axis=1, inplace=True)
+
+    price_history_len = 60
+
+    if len(df) > num_days + price_history_len:
+        df = df[len(df) - num_days - price_history_len:]
+    
+    close_prices = df.filter(['Close']).values
+    close_prices = SCALER.fit_transform(close_prices)
+
     x, y = [], []
-
     for i in range(price_history_len, len(close_prices)):
         x.append(close_prices[i - price_history_len : i, 0])
         y.append(close_prices[i, 0])
 
-    x, y = np.array(x), np.array(y)
-
-    return x[:train_data_len], y[:train_data_len], x[train_data_len:], y[train_data_len:]
-
-def create_model(input_shape):
-    model = keras.Sequential()
-    model.add(keras.layers.LSTM(units=50, return_sequences=True, input_shape=input_shape))
-    model.add(keras.layers.LSTM(units=50, return_sequences=False))
-    model.add(keras.layers.Dense(25))
-    model.add(keras.layers.Dense(1))
-    return model
-
-def plot_results(df, predictions):
-    plt.figure(figsize=(16,8))
-    plt.xlabel('Date', fontsize=18)
-    plt.ylabel('Close Price USD', fontsize=18)
-
-    train_len = len(df["Date"]) - len(predictions)
-
-    plt.plot(df['Date'][:train_len], df['Close'][:train_len])
-    plt.plot(df['Date'][train_len:], df['Close'][train_len:])
-    plt.plot(df['Date'][train_len:], predictions)
+    return df, np.array(x), np.array(y)
     
-    plt.xticks(np.arange(0, len(df['Date']) - 1, 1000))
-    plt.legend(['Train', 'Actual Value', 'Prediction'], loc='lower right')
+def plot_results(df, predictions):
 
-    #zoomed in plot
     plt.figure(figsize=(16,8))
     plt.xlabel('Date', fontsize=18)
     plt.ylabel('Close Price USD', fontsize=18)
 
-    plt.plot(df['Date'][train_len:], df['Close'][train_len:])
-    plt.plot(df['Date'][train_len:], predictions)
+    plt.plot(df["Date"], df["Close"])
+    plt.plot(df["Date"], predictions)
 
-    plt.xticks(np.arange(0, len(df['Date'][train_len:]) - 1, 200))
+    plt.xticks(np.arange(0, len(df['Date']) - 1, len(df['Date']) / 6))
     plt.legend(['Actual Value', 'Prediction'], loc='lower right')
 
     plt.show()
 
-def main():
-    df = pd.read_csv('../data/S&P500_data.csv')[18080:] # get data from 01/03/2000 onwards 18080
-    df.drop(['Open', 'High', 'Low', 'Adj Close', 'Volume'], axis=1, inplace=True)
-    print("Dataset length:", len(df["Close"]))
-    close_price_df = df.filter(['Close'])
-    close_prices = close_price_df.values
-
-    scaler = MinMaxScaler(feature_range=(0, 1)) 
-    data = scaler.fit_transform(close_prices)
-
-    price_history_len = 60
-    x_train, y_train, x_test, y_test = create_datasets(data, price_history_len)
-   
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
-    #create model
-    model = create_model((x_train.shape[1], 1))
-    model.compile(optimizer='adam', loss='mean_squared_error')
-    model.fit(x_train, y_train, batch_size=1, epochs=3)
-
-    model.evaluate(x_test, y_test)
-
-    #predict
-    predictions = model.predict(x_test) 
-    predictions = scaler.inverse_transform(predictions)
-
-    plot_results(df, predictions)
-    
-
-main()
-
-
-
+if __name__ == "__main__":
+    main()
